e01a834c3a1d1a498ce071cbc1d86deebb601c7c
amaanq <amaanq12@gmail.com>
2022-06-18 05:57:13 -0400

    removed .env, using cli flags, added video get/post endpoints (post more complete), ready to integrate with lower level APIs

---
+++ web-server/main.py
@@ -0,0 +1,14 @@
+"""
+This file loads the environment variables from the .env file.
+Afterwards, the Web Server is started.
+"""
+
+from argparser import create_arguments
+from webserver import WebServer
+
+if __name__ == "__main__":
+    parser = create_arguments()
+    args = parser.parse_args()
+
+    server = WebServer(args)
+    server.run()
