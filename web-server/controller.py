27a52dd8331c114d7e55083d159f629eff5550c8
PotatoPalooza <49171429+PotatoPalooza@users.noreply.github.com>
2022-08-14 18:19:04 -0400

    Services v2 (#32)
    * test 1: adding resource links
    
    * Annie's Weekly Status Update
    
    * squash
    
    * squash
    
    * added a readme
    
    * cleaning up mess
    
    * first MVC setup
    
    * requirements.txt and some models for db
    
    * sfm datamodel
    
    * moved models to webserver
    
    * Added website changes to this branch
    
    * Added website changes to this branch
    
    * Added file structure
    
    * Adding queue service - not finished - and UUID
    
    * Added stuff
    
    * Added data directory structure
    
    * RabbitMQ is sent link correctly
    
    * Changed RabbitMQ message sent to JSON
    
    * Threading might be working
    
    * Renamed some things
    
    * Removed junk code
    
    * May or may not be able to get files from external workers now; needs security overhaul though
    
    * Fixed syntax bug
    
    * Made external dummy server more robust
    
    * External dummy server now works to some extent
    
    * Get video request actually works now
    
    * Added cleanup dummy
    
    * Removed trash
    
    * Models for services done (#29)
    
    * merge models into scene
    
    * minimal model set up and working
    
    * Video reception and serving partially working
    
    * added ip_addr for clients
    
    * Pika uses connection with credentials
    
    * diagrams_added
    
    * ClientService and MongoClient are created in main, readded CORS to video GET request
    
    * Added new path to get nerf video for website
    
    * Changed from debug text, ouch
    
    * Finished sfm and nerf job submission to rabbitmq
    
    * update callback for nerf_out and sfm_out
    
    * fixing bugs with services
    
    * fixing que sfm processing
    
    * implemented requests to save data
    
    * comments added to webserver
    
    * finished async queues and wired  everything to start with main
    
    * finished rabbitmq services and mvc rewrite
    
    Co-authored-by: xxanxnie <jxu0029@bths.edu>
    Co-authored-by: Amaan Q <amaanq12@gmail.com>
    Co-authored-by: delexagon <delexagon@gmail.com>
    Co-authored-by: ytk0701 <106838767+ytk0701@users.noreply.github.com>

---
+++ web-server/controller.py
@@ -0,0 +1,103 @@
+import argparse
+import os
+import magic
+from uuid import uuid4, UUID
+
+from flask import Flask, request, make_response, send_file, send_from_directory
+
+from services.scene_service import ClientService
+
+def is_valid_uuid(value):
+    try:
+        UUID(str(value))
+        return True
+    except ValueError:
+        return False
+
+class WebServer:
+    def __init__(self, args: argparse.Namespace, cserv: ClientService) -> None:
+        self.app = Flask(__name__)
+        self.args = args
+        self.cservice = cserv
+
+    def run(self) -> None:
+        self.app.logger.setLevel(
+            int(self.args.log)
+        ) if self.args.log.isdecimal() else self.app.logger.setLevel(self.args.log)
+
+        self.add_routes()
+        
+        # TODO: Change this to work based on where Flask server starts. Also, use the actual ip address
+        ### self.sserv.base_url = request.remote_addr
+
+        self.app.run(port=self.args.port)
+
+    def add_routes(self) -> None:
+        @self.app.route("/")
+        def hello_world():
+            return "Do not access"
+
+        @self.app.route("/video", methods=["POST", "PUT"])
+        def recv_video():
+            """
+            Must decide if we want to hang here until video is done,
+            or return a 20x received and let the front-end query an endpoint
+            given a cookie to see if the video is done periodically
+            """
+            video_file = request.files.get("file")
+            print("VIDEO FILE", video_file)
+            # TODO: UUID4 is cryptographically secure on CPython, but this is not guaranteed in the specifications.
+            # Might want to change this.
+            # TODO: Don't assume videos are in mp4 format
+            uuid = self.cservice.handle_incoming_video(video_file)
+            if(uuid is None):
+                response = make_response("ERROR")
+                response.headers['Access-Control-Allow-Origin'] = '*'
+                return response
+            
+            # TODO: now pass to nerf/tensorf/colmap/sfm, and decide if synchronous or asynchronous
+            # will we use a db for cookies/ids?
+                
+            response = make_response(uuid)
+            response.headers['Access-Control-Allow-Origin'] = '*'
+
+            return response
+
+        @self.app.route("/video/<vidid>", methods=["GET"])
+        def send_video(vidid: str):
+            # TODO: Change routing to serve rendered videos
+            try:
+                if(is_valid_uuid(vidid)):
+                    path = os.path.join(os.getcwd(), "data/raw/videos/" + vidid + ".mp4")
+                    response = make_response(send_file(path, as_attachment=True))
+                else:
+                    response = make_response("Error: invalid UUID")
+            except Exception as e:
+                print(e)
+                response = make_response("Error: does not exist")
+           
+            return response
+            
+        @self.app.route("/nerfvideo/<vidid>", methods=["GET"])
+        def send_nerf_video(vidid: str):
+            ospath = None
+            status_str = "Processing"
+            if is_valid_uuid(vidid):
+                ospath = self.cservice.get_nerf_video_path(vidid)
+            # Could change this to return both
+            if ospath == None or not os.path.exists(ospath):
+                response = make_response(status_str)
+            else:
+                status_str = "Video ready"
+                response = make_response(send_file(ospath, as_attachment=True))
+                
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            return response
+
+        @self.app.route("/worker-data/<path:path>")
+        def send_worker_data(path):
+            # serves data directory for workers to pull any local data
+            # TODO make this access secure
+            return send_from_directory('data',path[5:])
+            
+        
---
+++ web-server/webserver_model.md
@@ -0,0 +1,16 @@
+Webserver diagram
+
+![image](https://cdn.discordapp.com/attachments/983855301251989584/1005171261804134480/Webserver.png)
+
+## Controller
+
+Controller is written with Flask to handle basic HTTP methods: post and get. Controller routes datas to Services and Frontend.
+
+## Services
+
+
+## Model
+
+Models stores the raw data in the format similar to MongoDB.
+
+
