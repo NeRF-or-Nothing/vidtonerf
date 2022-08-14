f6f559a2fdf828a2f7e8e2b33d40e469c514fea4
PotatoPalooza <49171429+PotatoPalooza@users.noreply.github.com>
2022-08-14 18:19:04 -0400

    Services v2 (#32)
    * test 1: adding resource links
    
    * Annie's Weekly Status Update
    
    * squash
    
    * squash
    
    * added a readme
    
    * cleaning up mess
    
    * first MVC setup
    
    * requirements.txt and some models for db
    
    * sfm datamodel
    
    * moved models to webserver
    
    * Added website changes to this branch
    
    * Added website changes to this branch
    
    * Added file structure
    
    * Adding queue service - not finished - and UUID
    
    * Added stuff
    
    * Added data directory structure
    
    * RabbitMQ is sent link correctly
    
    * Changed RabbitMQ message sent to JSON
    
    * Threading might be working
    
    * Renamed some things
    
    * Removed junk code
    
    * May or may not be able to get files from external workers now; needs security overhaul though
    
    * Fixed syntax bug
    
    * Made external dummy server more robust
    
    * External dummy server now works to some extent
    
    * Get video request actually works now
    
    * Added cleanup dummy
    
    * Removed trash
    
    * Models for services done (#29)
    
    * merge models into scene
    
    * minimal model set up and working
    
    * Video reception and serving partially working
    
    * added ip_addr for clients
    
    * Pika uses connection with credentials
    
    * diagrams_added
    
    * ClientService and MongoClient are created in main, readded CORS to video GET request
    
    * Added new path to get nerf video for website
    
    * Changed from debug text, ouch
    
    * Finished sfm and nerf job submission to rabbitmq
    
    * update callback for nerf_out and sfm_out
    
    * fixing bugs with services
    
    * fixing que sfm processing
    
    * implemented requests to save data
    
    * comments added to webserver
    
    * finished async queues and wired  everything to start with main
    
    * finished rabbitmq services and mvc rewrite
    
    Co-authored-by: xxanxnie <jxu0029@bths.edu>
    Co-authored-by: Amaan Q <amaanq12@gmail.com>
    Co-authored-by: delexagon <delexagon@gmail.com>
    Co-authored-by: ytk0701 <106838767+ytk0701@users.noreply.github.com>

---
+++ web-server/services/scene_service.py
@@ -0,0 +1,56 @@
+import os
+import requests
+import json
+from models.scene import SceneManager, Video
+from services.queue_service import RabbitMQService
+from uuid import uuid4, UUID
+from werkzeug.utils import secure_filename
+
+from pymongo import MongoClient
+
+class ClientService:
+    def __init__(self, manager: SceneManager, rmqservice: RabbitMQService):
+        self.manager = manager
+        self.rmqservice = rmqservice
+        
+        #self.queue = queue
+
+    def handle_incoming_video(self, video_file):
+        # receive video and check for validity
+        file_name = secure_filename(video_file.filename)
+        if file_name == '':
+            print("ERROR: file not received")
+            return None
+
+        file_ext = os.path.splitext(file_name)[1]
+        if file_ext != ".mp4":
+            print("ERROR: improper file extension uploaded")
+            return None
+
+        # generate new id and save to file with db record
+        uuid = str(uuid4())
+        video_name = uuid + ".mp4"
+        videos_folder = "data/raw/videos"
+        video_file_path = os.path.join(videos_folder,video_name)
+        
+        video_file.save(video_file_path)
+
+        video = Video(video_file_path)
+        self.manager.set_video(uuid, video)
+
+        # create rabbitmq job for sfm
+        #TODO
+        self.rmqservice.publish_sfm_job(uuid, video)
+
+        return uuid
+
+    # Returns a string describing the status of the video in the database
+    # along with a path to the final video, if available
+    def get_nerf_video_path(self, uuid):
+        # TODO: depend on mongodb to load file path
+        # return None if not found
+        nerf = self.manager.get_nerf(uuid)
+        if nerf:
+            return ("Video ready", nerf.rendered_video_path)
+        return None
+        
