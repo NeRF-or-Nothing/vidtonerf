018d2c84b29d9c50e2db994bc266c88be50b1396
Michael Chen <michaelchen@gonkulator-86.dynamic2.rpi.edu>
2023-02-09 17:21:51 -0500

    Adding Command Line --run_mode for docker_compose yml and dockerfile

--- colmap/main.py
+++ colmap/main.py
@@ -11,6 +11,8 @@ import json
 import time
 from multiprocessing import Process
 import os
+import argparse
+import sys
 
 
 app = Flask(__name__)
@@ -72,61 +74,69 @@ def run_full_sfm_pipeline(id,video_file_path, input_data_dir, output_data_dir):
 
     return motion_data, imgs_folder
 
-def colmap_worker():
-    rabbitmq_domain = "rabbitmq"
-    credentials = pika.PlainCredentials('admin', 'password123')
-    parameters = pika.ConnectionParameters(rabbitmq_domain, 5672, '/', credentials,heartbeat=300)
+def colmap_worker(use_rabbitmq = False):
 
-    connection = pika.BlockingConnection(parameters)
-    channel = connection.channel()
-    channel.queue_declare(queue='sfm-in')
-    channel.queue_declare(queue='sfm-out')
     input_data_dir = "data/inputs/"
     output_data_dir = "data/outputs/"
     Path(f"{input_data_dir}").mkdir(parents=True, exist_ok=True)
     Path(f"{output_data_dir}").mkdir(parents=True, exist_ok=True)
 
-    def process_colmap_job(ch, method, properties, body):
-        print("Starting New Job")
-        print(body.decode())
-        job_data = json.loads(body.decode())
-        id = job_data["id"]
-        print(f"Running New Job With ID: {id}")
-        
-        #TODO: Handle exceptions and enable steaming to make safer 
-        video = requests.get(job_data['file_path'], timeout=10)
-        print("Web server pinged")
-        video_file_path = f"{input_data_dir}{id}.mp4"
-        open(video_file_path,"wb").write(video.content)
-        print("Video downloaded")
-        
-        # RUNS COLMAP AND CONVERSION CODE
-        motion_data, imgs_folder = run_full_sfm_pipeline(id, video_file_path, input_data_dir, output_data_dir)
+    if use_rabbitmq: 
+        rabbitmq_domain = "rabbitmq"
+        credentials = pika.PlainCredentials('admin', 'password123')
+        parameters = pika.ConnectionParameters(rabbitmq_domain, 5672, '/', credentials,heartbeat=300)
 
-        # create links to local data to serve
-        for i,frame in enumerate(motion_data["frames"]):
-            file_name = frame["file_path"]
-            file_path = os.path.join(imgs_folder,file_name)
-            file_url = to_url(file_path)
-            motion_data["frames"][i]["file_path"] = file_url
+        connection = pika.BlockingConnection(parameters)
+        channel = connection.channel()
+        channel.queue_declare(queue='sfm-in')
+        channel.queue_declare(queue='sfm-out')
 
-        json_motion_data = json.dumps(motion_data)
-        channel.basic_publish(exchange='', routing_key='sfm-out', body=json_motion_data)
+        def process_colmap_job(ch, method, properties, body):
+            print("Starting New Job")
+            print(body.decode())
+            job_data = json.loads(body.decode())
+            id = job_data["id"]
+            print(f"Running New Job With ID: {id}")
+            
+            #TODO: Handle exceptions and enable steaming to make safer 
+            video = requests.get(job_data['file_path'], timeout=10)
+            print("Web server pinged")
+            video_file_path = f"{input_data_dir}{id}.mp4"
+            open(video_file_path,"wb").write(video.content)
+            print("Video downloaded")
+            
+            # RUNS COLMAP AND CONVERSION CODE
+            motion_data, imgs_folder = run_full_sfm_pipeline(id, video_file_path, input_data_dir, output_data_dir)
 
-        # confirm to rabbitmq job is done
-        ch.basic_ack(delivery_tag=method.delivery_tag)
-        print("Job complete")
+            # create links to local data to serve
+            for i,frame in enumerate(motion_data["frames"]):
+                file_name = frame["file_path"]
+                file_path = os.path.join(imgs_folder,file_name)
+                file_url = to_url(file_path)
+                motion_data["frames"][i]["file_path"] = file_url
 
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(queue='sfm-in', on_message_callback=process_colmap_job)
-    channel.start_consuming()
-    print("should not get here")
+            json_motion_data = json.dumps(motion_data)
+            channel.basic_publish(exchange='', routing_key='sfm-out', body=json_motion_data)
+
+            # confirm to rabbitmq job is done
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+            print("Job complete")
+
+        channel.basic_qos(prefetch_count=1)
+        channel.basic_consume(queue='sfm-in', on_message_callback=process_colmap_job)
+        channel.start_consuming()
+        print("should not get here")
+    else: 
+        pass
 
 
 if __name__ == "__main__":
-    flaskProcess = Process(target=start_flask, args= ())
-    nerfProcess = Process(target=colmap_worker, args= ())
-    flaskProcess.start()
-    nerfProcess.start()
-    flaskProcess.join()
-    nerfProcess.join()
+    parser = argparse.ArgumentParser(description=("Run locally or on RabbitMQ"))
+    if "--run_mode" in sys.argv: 
+        print("Run_Mode Test")
+    # flaskProcess = Process(target=start_flask, args= ())
+    # nerfProcess = Process(target=colmap_worker, args= ())
+    # flaskProcess.start()
+    # nerfProcess.start()
+    # flaskProcess.join()
+    # nerfProcess.join()``
