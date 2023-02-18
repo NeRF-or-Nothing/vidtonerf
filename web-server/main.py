17b1c11ab17304515d8aaf71ba3dff47681700ca
Eric Nelson <49171429+PotatoPalooza@users.noreply.github.com>
2023-02-17 19:51:51 -0500

    General cleanup (#67)
    Removing redundant code, reorganizing declarations, adding TODOs.
    
    Co-authored-by: rougejaw <61851922+rougejaw@users.noreply.github.com>

--- web-server/main.py
+++ web-server/main.py
@@ -15,14 +15,13 @@ from pymongo import MongoClient
 
 def main():
     print("Starting web-app...")
-    # MongoDB client shared across threads since it is thread safe
-    mongodb_domain = "mongodb"
-    m_client = MongoClient(host=mongodb_domain,port=27017,username="admin",password="password123")
+    
     parser = create_arguments()
     args = parser.parse_args()
     
     # Shared Database manager <from models>
-    scene_man = SceneManager(m_client)
+    # SceneManager shared across threads since it is thread safe
+    scene_man = SceneManager()
 
     # Rabbitmq service to post new jobs to the workers <from services>
     rmq_service = RabbitMQService()
