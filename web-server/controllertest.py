f2eed54daccfa9c528d3f3cc90c668f6ea9b6027
Rcheng2003 <64754199+Rcheng2003@users.noreply.github.com>
2023-03-24 22:21:43 -0400

    testing

--- web-server/controllertest.py
+++ web-server/controllertest.py
@@ -1,9 +1,22 @@
 from flask import Flask, request, make_response, send_file, send_from_directory, url_for
+from controller import WebServer;
+from services.scene_service import ClientService;
+from models.scene import SceneManager
+from services.queue_service import RabbitMQService
+import argparse
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "this is an html string!"
+
+SM = SceneManager()
+RQ = RabbitMQService()
+
+CS = ClientService(SM,RQ)
+
+test = WebServer(argparse.Namespace, CS)
+test.run()
 
 app.run(host="0.0.0.0", port = 4000)
