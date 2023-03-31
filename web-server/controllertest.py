f7f8cd9b04f4b8b8376b30986755f7a6a870f252
Tashi Sherpa <68435081+TSherpa10@users.noreply.github.com>
2023-03-31 17:04:42 -0400

    testing template done, edited error status for pytest ease

--- web-server/controllertest.py
+++ web-server/controllertest.py
@@ -1,4 +1,46 @@
 from flask import Flask, request, make_response, send_file, send_from_directory, url_for
-from controller import WebServer;
-import pytest 
+from controller import WebServer
+from services.scene_service import ClientService
+from models.scene import SceneManager
+from services.queue_service import RabbitMQService
 import argparse
+import pytest
+
+app = Flask(__name__)
+
+
+def testAddRoutes():
+
+    check = False
+
+    response = app.test_client.get('/video/publish')
+    if (response.status == 404):
+        check = True
+
+    assert(check == True)
+
+    check = False
+
+    response = app.test_client.get('/video')
+    if (response.status == 404):
+        check = True
+
+    assert(check == True)
+
+    check = False
+
+    response = app.test_client.get('/video/<vidid>')
+    if (response.status == 404):
+        check = True
+
+    assert(check == True)
+
+    check = False
+
+    response = app.test_client.get('/nerfvideo/<vidid>')
+    if (response.status == 404):
+        check = True
+
+    assert(check == True)
+
+    return "Test Completed!"
