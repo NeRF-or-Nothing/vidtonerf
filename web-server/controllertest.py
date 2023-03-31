82b1c8df27e50d6ad848fdc0b53704bd4269c6f0
Tashi Sherpa <68435081+TSherpa10@users.noreply.github.com>
2023-03-31 17:13:08 -0400

    merge incoming :((

--- web-server/controllertest.py
+++ web-server/controllertest.py
@@ -1,13 +1,12 @@
 from flask import Flask, request, make_response, send_file, send_from_directory, url_for
-from controller import WebServer
-from services.scene_service import ClientService
+from controller import WebServer;
+from services.scene_service import ClientService;
 from models.scene import SceneManager
 from services.queue_service import RabbitMQService
 import argparse
 import pytest
 
 app = Flask(__name__)
-
 
 def testAddRoutes():
 
@@ -16,7 +15,7 @@ def testAddRoutes():
     response = app.test_client.get('/video/publish')
     if (response.status == 404):
         check = True
-
+    
     assert(check == True)
 
     check = False
@@ -44,3 +43,8 @@ def testAddRoutes():
     assert(check == True)
 
     return "Test Completed!"
+
+
+
+
+
