8afe5bc5d9a5dc1991d689f5fc6f55e86447c790
dianadah <kangd5@rpi.edu>
2023-04-08 18:09:28 -0400

    Worker class in scene.py (#86)
    Created Worker class with "_id", "api_key", "owner_id", and "type".
    
    Co-authored-by: Eric Nelson <49171429+PotatoPalooza@users.noreply.github.com>

--- web-server/models/scene.py
+++ web-server/models/scene.py
@@ -203,6 +203,46 @@ def scene_to_dict(x: Scene) -> Any:
     return to_class(Scene, x)
 
 
+
+@dataclass
+class Worker:
+    id: Optional[str] = None
+    api_key: Optional[str] = None
+    owner_id: Optional[str] = None
+    type: Optional[str] = None
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'Worker':
+        assert isinstance(obj, dict)
+        id = from_union([from_str, from_none], obj.get("_id"))
+        api_key = from_union([from_str, from_none], obj.get("api_key"))
+        owner_id = from_union([from_str, from_none], obj.get("owner_id"))
+        type = from_union([from_str, from_none], obj.get("type"))
+        return Worker(id, api_key, owner_id, type)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        if self.id is not None:
+            result["_id"] = from_union([from_str, from_none], self.id)
+        if self.api_key is not None:
+            result["api_key"] = from_union([from_str, from_none], self.api_key)
+        if self.owner_id is not None:
+            result["owner_id"] = from_union([from_str, from_none], self.owner_id)
+        if self.type is not None:
+            result["type"] = from_union([from_str, from_none], self.type)
+        return result
+
+
+def worker_from_dict(s: Any) -> Worker:
+    return Worker.from_dict(s)
+
+
+def worker_to_dict(x: Worker) -> Any:
+    return to_class(Worker, x)
+
+
+
+
 # api_key owner
 @dataclass
 class User:
