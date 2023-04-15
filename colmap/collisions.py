bf429ecd96dbe7c54c968f8cea6bd28486f3bbde
AbramK24 <abramkonzel@gmail.com>
2023-04-14 23:41:20 -0400

    Update collisions.py
    Finalize implementation of counter function

--- colmap/collisions.py
+++ colmap/collisions.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import json
 
-def counter(matrices, lims):
+def counter(extrinsics, intrinsic, lims):
 
     xlim = lims[0]
     ylim = lims[1]
     zlim = lims[2]
 
-    n = np.array([
+    p_0 = np.array([
         [xlim[0], 0, 0],
         [xlim[1], 0, 0],
         [0, ylim[0], 0],
@@ -18,7 +18,7 @@ def counter(matrices, lims):
         [0, 0, zlim[1]]
     ])
 
-    p_0 = np.array([
+    n = np.array([
         [-1, 0, 0],
         [1, 0, 0],
         [0, -1, 0],
@@ -27,17 +27,58 @@ def counter(matrices, lims):
         [0, 0, 1]
     ])    
 
-    for matrix in matrices:
+    count_tot = 0
+    for matrix in extrinsics:
 
-        l_0 = matrix[:, :, 3]
-        l = np.array([0, 0, -1, 1]) @ matrix.T
+        l_0 = matrix[:, 3][:3]
+        l = np.linalg.inv(intrinsic) @ np.array([0, 0, 1])
+        l = np.append(l, 1)
+        l = matrix @ l
+        l = l[:3]
+        
+        count_int = 0
+        for i in range(0, 6):
 
+            d = ((p_0[i] - l_0).T @ n[i])/(l.T @ n[i])
+            p = l_0 + l*d
+
+            if i == 1 or i == 2:
+
+                if (p[1] > ylim[0] and p[1] < ylim[1]) and (p[2] > zlim[0] and p[2] < zlim[1]):
+
+                    count_int += 1
+
+            if i == 3 or i == 4:
+
+                if (p[0] > xlim[0] and p[0] < xlim[1]) and (p[2] > zlim[0] and p[2] < zlim[1]):
+                    
+                    count_int += 1
+
+            if i == 5 or i == 6:
+
+                if (p[0] > xlim[0] and p[0] < xlim[1]) and (p[1] > ylim[0] and p[1] < ylim[1]):
+
+                    count_int += 1
+        
+        if count_int > 0:
+
+            count_tot += 1
+
+    return count_tot
 
 if __name__ == '__main__':
 
     transforms_data = json.load(open('./data/outputs/Local_Test/transforms_data.json'))
     extrinsics = np.array([np.array(frame['extrinsic_matrix']) for frame in transforms_data['frames']])
     intrinsic = transforms_data['intrinsic_matrix']
+
+    xlim = [-1, 1]
+    ylim = [-1, 1]
+    zlim = [-1, 1]
+
+    lims = np.array([xlim, ylim, zlim])
+
+    print(counter(extrinsics, intrinsic, lims)/len(extrinsics))
 
     #Plotting camera path
     fig = plt.figure(figsize=(10, 10))
@@ -72,11 +113,7 @@ if __name__ == '__main__':
         camera_loc = np.array([0, 0, 0, 1])
         vertex_transformed = camera_loc @ matrix.T
 
-        ax.plot([vertex_transformed[0], -vertex_transformed[0]], [vertex_transformed[1], -vertex_transformed[1]], [vertex_transformed[2], -vertex_transformed[2]], c = 'black', alpha = 0.1)
-
-    xlim = [-1, 1]
-    ylim = [-1, 1]
-    zlim = [-1, 1]
+        ax.plot([vertex_transformed[0], 0.5*vertex_transformed[0]], [vertex_transformed[1], 0.5*vertex_transformed[1]], [vertex_transformed[2], 0.5*vertex_transformed[2]], c = 'black', alpha = 0.1)
 
     box = np.array([
         [[xlim[0], ylim[0], zlim[0]], [xlim[1], ylim[0], zlim[0]], [xlim[1], ylim[0], zlim[1]], [xlim[0], ylim[0], zlim[1]]], 
