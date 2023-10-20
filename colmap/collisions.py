befedf58e3699fa6de12d3853feecd3207d60185
Gabe C <113395303+GabeVC@users.noreply.github.com>
2023-10-20 18:31:46 -0400

    Collisions update
    now plots vectors and shows direction

--- colmap/collisions.py
+++ colmap/collisions.py
@@ -83,3 +83,72 @@ if __name__ == '__main__':
     transforms_data = json.load(open('./data/outputs/Local_Test/transforms_data.json'))
     extrinsics = np.array([np.array(frame['extrinsic_matrix']) for frame in transforms_data['frames']])
     intrinsic = transforms_data['intrinsic_matrix']
+    
+    xlim = [-1, 1]
+    ylim = [-1, 1]
+    zlim = [-1, 1]
+
+    lims = np.array([xlim, ylim, zlim])
+
+    out = counter(extrinsics, intrinsic, lims)
+    print(out[0])
+
+    #Plotting camera path
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(projection='3d')
+    ax.set_xlim([-5, 5])
+    ax.set_ylim([-5, 5])
+    ax.set_zlim([-5, 5])
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+
+    focal_len_scaled = 1
+    aspect_ratio = 0.3
+
+    for matrix in extrinsics:
+
+        vertex_camera = np.array([[0, 0, 0, 1],
+                                [focal_len_scaled * aspect_ratio, -focal_len_scaled * aspect_ratio, focal_len_scaled, 1],
+                                [focal_len_scaled * aspect_ratio, focal_len_scaled * aspect_ratio, focal_len_scaled, 1],
+                                [-focal_len_scaled * aspect_ratio, focal_len_scaled * aspect_ratio, focal_len_scaled, 1],
+                                [-focal_len_scaled * aspect_ratio, -focal_len_scaled * aspect_ratio, focal_len_scaled, 1]])
+
+        vertex_transformed = vertex_camera @ matrix.T
+
+        meshes = [[vertex_transformed[0, :-1], vertex_transformed[1][:-1], vertex_transformed[2, :-1]],
+                                [vertex_transformed[0, :-1], vertex_transformed[2, :-1], vertex_transformed[3, :-1]],
+                                [vertex_transformed[0, :-1], vertex_transformed[3, :-1], vertex_transformed[4, :-1]],
+                                [vertex_transformed[0, :-1], vertex_transformed[4, :-1], vertex_transformed[1, :-1]],
+                                [vertex_transformed[1, :-1], vertex_transformed[2, :-1], vertex_transformed[3, :-1], vertex_transformed[4, :-1]]]
+        ax.add_collection3d(Poly3DCollection(meshes, linewidths=0.3, alpha=0.15, color = 'red'))
+
+        l = matrix @ np.array([0, 0, -1, 1], )
+        l = l[:3]
+        l_0 = matrix[:, 3][:3]
+
+        #uncomment this to plot direction vectors
+        ax.plot([l_0[0], l[0]], [l_0[1], l[1]], [l_0[2], l[2]], color = 'black', alpha = 0.5)
+
+    box = np.array([
+        [[xlim[0], ylim[0], zlim[0]], [xlim[1], ylim[0], zlim[0]], [xlim[1], ylim[0], zlim[1]], [xlim[0], ylim[0], zlim[1]]], 
+        [[xlim[0], ylim[0], zlim[0]], [xlim[0], ylim[1], zlim[0]], [zlim[0], ylim[1], zlim[1]], [zlim[0], ylim[0], zlim[1]]],
+        [[xlim[0], ylim[1], zlim[0]], [xlim[0], ylim[1], zlim[1]], [xlim[1], ylim[1], zlim[1]], [xlim[1], ylim[1], zlim[0]]],
+        [[xlim[1], ylim[0], ylim[0]], [xlim[1], ylim[1], zlim[0]], [xlim[1], ylim[1], zlim[1]], [xlim[1], ylim[0], zlim[1]]],
+        [[xlim[0], ylim[0], zlim[0]], [xlim[1], ylim[0], zlim[0]], [xlim[1], ylim[1], zlim[0]], [xlim[0], ylim[1], zlim[0]]],
+        [[xlim[0], ylim[0], zlim[1]], [xlim[1], ylim[0], zlim[1]], [xlim[1], ylim[1], zlim[1]], [xlim[0], ylim[1], zlim[1]]]
+        ])
+    ax.add_collection3d(Poly3DCollection(box, alpha = 0.5))
+
+    C = extrinsics[:, :, 3]
+    C = np.moveaxis(C, 0, 1)
+
+    ax.scatter(0, 0, 0, color = 'black', marker = '+', )
+    ax.scatter(C[0], C[1], C[2], c = np.arange(len(C[0])))
+
+    for point in out[1]:
+        ax.scatter(point[0], point[1], point[2], color = 'black', marker = ',')
+
+
+
+    plt.show()
