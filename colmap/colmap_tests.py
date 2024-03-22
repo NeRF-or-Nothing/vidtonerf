bc60d8c0239dbd6eb363b1bb88fad7180d70afc2
Aaron Verkleeren <109297400+Averkleeren@users.noreply.github.com>
2024-03-22 16:54:43 -0400

    Added more colmap tests 	modified:   colmap/colmap_tests.py

--- colmap/colmap_tests.py
+++ colmap/colmap_tests.py
@@ -17,6 +17,39 @@ class TestMatrixFunctions(unittest.TestCase):
         self.assertAlmostEqual(pitch, 0.0)
         self.assertAlmostEqual(yaw, 1.5707963267948966)
 
-    
+    def test_quaternion_rotation_matrix(self):
+        # Test case 1
+        rotation_matrix = quaternion_rotation_matrix(0.5, 0.5, 0.5, 0.5)
+        expected_matrix = np.array([[0.0, 0.0, 1.0],
+                                    [1.0, 0.0, 0.0],
+                                    [0.0, 1.0, 0.0]])
+        np.testing.assert_array_almost_equal(rotation_matrix, expected_matrix)
+
+        # Test case 2
+        rotation_matrix = quaternion_rotation_matrix(0.0, 0.0, 0.0, 1.0)
+        expected_matrix = np.array([[-1.0, 0.0, 0.0],
+                                    [0.0, -1.0, 0.0],
+                                    [0.0, 0.0, 1.0]])
+        np.testing.assert_array_almost_equal(rotation_matrix, expected_matrix)
+
+    def test_rotation_matrix_from_vectors(self):
+        # Test case 1
+        vec1 = np.array([1.0, 0.0, 0.0])
+        vec2 = np.array([0.0, 1.0, 0.0])
+        rotation_matrix = rotation_matrix_from_vectors(vec1, vec2)
+        expected_matrix = np.array([[0.0, -1.0, 0.0],
+                                    [1.0, 0.0, 0.0],
+                                    [0.0, 0.0, 1.0]])
+        np.testing.assert_array_almost_equal(rotation_matrix, expected_matrix)
+
+        # Test case 2
+        vec1 = np.array([1.0, 0.0, 0.0])
+        vec2 = np.array([0.0, 0.0, 1.0])
+        rotation_matrix = rotation_matrix_from_vectors(vec1, vec2)
+        expected_matrix = np.array([[0.0, 0.0, -1.0],
+                                    [0.0, 1.0, 0.0],
+                                    [1.0, 0.0, 0.0]])
+        np.testing.assert_array_almost_equal(rotation_matrix, expected_matrix)
+
 if __name__ == '__main__':
     unittest.main()
