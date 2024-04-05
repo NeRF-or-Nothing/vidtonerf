d7a4fc06fbe7c8f8d00e3a39027dbc132cf33c3a
simon <stgrb2002@gmail.com>
2024-04-05 17:27:50 -0400

    Change blockConnection to spawn a thread to run nerf pipeline and use threadsafe ack/publish callbacks

--- TensoRF/main.py
+++ TensoRF/main.py
@@ -12,6 +12,8 @@ from dataLoader import sfm2nerf
 from opt import config_parser
 from worker import train_tensorf, render_novel_view
 from dotenv import load_dotenv
+import functools
+import threading
 
 
 app = Flask(__name__)
@@ -28,65 +30,83 @@ def start_flask():
     global app
     app.run(host="0.0.0.0", port=5200, debug=True)
 
+def on_message(channel, method, header, body, args):
+    thrds = args
+    delivery_tag = method.delivery_tag
+    
+    #t = Thread(target = run_nerf_job, args=(channel, method, header, body))
+    t = threading.Thread(target = run_nerf_job, args=(channel, method, delivery_tag, body))
+    t.start()
+    thrds.append(t)
+
+def ack_publish_message(channel, delivery_tag, body):
+    if body:
+        channel.basic_publish(exchange='', routing_key='nerf-out', body=body)
+    channel.basic_ack(delivery_tag=delivery_tag)
+    
+def run_nerf_job(channel, method, properties, body):
+    
+    args = config_parser(
+        "--config configs/localworkerconfig_testsimon.txt")
+
+    nerf_data = json.loads(body.decode())
+    id = nerf_data["id"]
+    width = nerf_data["vid_width"]
+    height = nerf_data["vid_height"]
+    intrinsic_matrix = nerf_data["intrinsic_matrix"]
+    frames = nerf_data["frames"]
+
+    input_dir = Path(f"data/sfm_data/{id}")
+    os.makedirs(input_dir, exist_ok=True)
+
+    for i, fr_ in enumerate(frames):
+        # Save copy of motion data
+        url = fr_["file_path"]
+        img = requests.get(url)
+        fr_["file_path"] = f"{i}.png"
+        img_file_path = input_dir / fr_["file_path"]
+        img_file_path.write_bytes(img.content)
+
+    # Save copy of transform data
+    input_train = input_dir / f"transforms_train.json"
+    input_render = input_dir / f"transforms_render.json"
+    input_train.write_text(json.dumps(nerf_data, indent=4))
+    input_render.write_text(json.dumps(nerf_data, indent=4))
+
+    # Run TensoRF algorithm, creates sfm2nerf datatype for training
+    args.datadir += f"/{id}"
+    args.expname = id
+    logfolder, tensorf_model = train_tensorf(args)
+    local_video_path = render_novel_view(args, logfolder, tensorf_model)
+
+    # Save model and video to nerf_data for retrieval
+    out_model_path = Path(f"data/nerf_data/{id}/model.th")
+    out_video_path = Path(f"data/nerf_data/{id}/video.mp4")
+    os.makedirs(f"data/nerf_data/{id}", exist_ok=True)
+    shutil.copy(f"{logfolder}/imgs_render_all/video.mp4", out_video_path)
+    shutil.copy(f"{logfolder}/{id}.th", out_model_path)
+
+    out_model_path = base_url + str(out_model_path)
+    out_video_path = base_url + str(out_video_path)
+
+    nerf_output_object = {
+        "id": id,
+        "model_filepath": out_model_path,
+        "rendered_video_path": out_video_path
+    }
+
+    # Use thread safe callback to ack message and publish nerf_output_object 
+    callback = functools.partial(
+        ack_publish_message, 
+        channel, 
+        method.delivery_tag, 
+        json.dumps(nerf_output_object))
+    
+    channel.connection.add_callback_threadsafe(callback)
+
 
 def nerf_worker():
     print("Starting nerf_worker!")
-
-    def process_nerf_job(ch, method, properties, body):
-        args = config_parser("--config configs/localworkerconfig_testsimon.txt")
-
-        nerf_data = json.loads(body.decode())
-        id = nerf_data["id"]  
-        width = nerf_data["vid_width"]
-        height = nerf_data["vid_height"]
-        intrinsic_matrix = nerf_data["intrinsic_matrix"]
-        frames = nerf_data["frames"]
-        
-        input_dir = Path(f"data/sfm_data/{id}")
-        os.makedirs(input_dir, exist_ok=True)
-        
-        for i, fr_ in enumerate(frames):
-            # Save copy of motion data
-            url = fr_["file_path"]
-            img = requests.get(url)
-            fr_["file_path"] = f"{i}.png"
-            img_file_path = input_dir / fr_["file_path"]
-            img_file_path.write_bytes(img.content)
-
-        # Save copy of transform data
-        input_train = input_dir / f"transforms_train.json"
-        input_render = input_dir / f"transforms_render.json"
-        input_train.write_text(json.dumps(nerf_data, indent=4))
-        input_render.write_text(json.dumps(nerf_data, indent=4))
-        
-        # Run TensoRF algorithm, creates sfm2nerf datatype for training
-        args.datadir += f"/{id}"
-        args.expname = id
-        logfolder, tensorf_model = train_tensorf(args)
-        local_video_path = render_novel_view(args, logfolder, tensorf_model)
-
-        # Save model and video to nerf_data for retrieval
-        out_model_path = Path(f"data/nerf_data/{id}/model.th")
-        out_video_path = Path(f"data/nerf_data/{id}/video.mp4")
-        os.makedirs(f"data/nerf_data/{id}", exist_ok=True)
-        shutil.copy(f"{logfolder}/imgs_render_all/video.mp4", out_video_path)
-        shutil.copy(f"{logfolder}/{id}.th", out_model_path)
-        
-        out_model_path = base_url + str(out_model_path)
-        out_video_path = base_url + str(out_video_path)
-        
-        nerf_output_object = {
-          "id" : id,
-          "model_filepath" : out_model_path, 
-          "rendered_video_path" : out_video_path
-        }
-        combined_output = json.dumps(nerf_output_object)
-        
-        channel.basic_publish(exchange='', routing_key='nerf-out', body= combined_output)
-        ch.basic_ack(delivery_tag=method.delivery_tag)
-        print(f"Nerf job {id} completed!")
-    
-    
     # TODO: Communicate with rabbitmq server on port defined in web-server arguments
     load_dotenv()
     rabbitmq_domain = "rabbitmq"
@@ -103,20 +123,26 @@ def nerf_worker():
             raise Exception(
                 "nerf_worker took too long to connect to rabbitmq")
         try:
+            threads = []
             connection = pika.BlockingConnection(parameters)
             channel = connection.channel()
             channel.queue_declare(queue='nerf-in')
             channel.queue_declare(queue='nerf-out')
 
-            # Will block and call process_nerf_job repeatedly
+            # Will block and create a thread to process nerf_job
             channel.basic_qos(prefetch_count=1)
-            channel.basic_consume(queue='nerf-in', on_message_callback=process_nerf_job, auto_ack=False)
+            on_message_callback = functools.partial(on_message, args = (threads))
+            channel.basic_consume(queue='nerf-in', on_message_callback=on_message_callback, auto_ack=False)
             try:
                 channel.start_consuming()
             except KeyboardInterrupt:
                 channel.stop_consuming()
                 connection.close()
                 break
+            
+            for thread in threads:
+                thread.join()
+                
         except pika.exceptions.AMQPConnectionError:
             continue
     
